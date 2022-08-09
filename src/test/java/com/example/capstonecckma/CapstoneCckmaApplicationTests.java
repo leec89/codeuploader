8386154bf5522c67981a8aaac87b7f1ef1e6cf4e
leec89 <leec89@gmail.com>
2022-08-09 09:51:01 -0500

    first commit

---
+++ src/test/java/com/example/capstonecckma/CapstoneCckmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.capstonecckma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneCckmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
