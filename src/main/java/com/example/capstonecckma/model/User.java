64af0545f4062eb871f1af4b46940e6b9235711f
Michael Dickens <dickens.michael64@gmail.com>
2022-08-11 15:35:53 -0500

    updated UserController to add Get/Post mappings for registering

--- src/main/java/com/example/capstonecckma/model/User.java
+++ src/main/java/com/example/capstonecckma/model/User.java
@@ -30,13 +30,11 @@ public class User {
 
     public User() {
     }
-
     public User(String username, String email, String password) {
         this.username = username;
         this.email = email;
         this.password = password;
     }
-
     public User(User copy) {
         id = copy.id; // This line is SUPER important!
         email = copy.email;
@@ -44,23 +42,18 @@ public class User {
         password = copy.password;
         resources = copy.resources;
     }
-
     public void setId(long id) {
         this.id = id;
     }
-
     public List<Resource> getPosts() {
         return resources;
     }
-
     public void setResources(List<Resource> resources) {
         this.resources = resources;
     }
-
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
@@ -68,23 +61,18 @@ public class User {
     public String getUsername() {
         return username;
     }
-
     public void setUsername(String username) {
         this.username = username;
     }
-
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
-
     public String getPassword() {
         return password;
     }
-
     public void setPassword(String password) {
         this.password = password;
     }
