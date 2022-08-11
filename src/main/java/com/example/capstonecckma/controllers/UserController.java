90bccb68972d85345ccbcd72e51b22a4ae30bfc4
leec89 <leec89@gmail.com>
2022-08-11 10:09:44 -0500

    user model, controller, repository initial

---
+++ src/main/java/com/example/capstonecckma/controllers/UserController.java
@@ -0,0 +1,19 @@
+package com.example.capstonecckma.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+
+    private ResourceRepository resourceDao;
+    private UserRepository userDao;
+    private PasswordEncoder passwordEncoder;
+    private EmailService emailService;
+
+    public UserController(ResourceRepository resourceDao, UserRepository userDao, PasswordEncoder passwordEncoder, EmailService emailService) {
+        this.resourceDao = resourceDao;
+        this.userDao = userDao;
+        this.passwordEncoder = passwordEncoder;
+        this.emailService= emailService;
+    }
+}
---
+++ src/main/java/com/example/capstonecckma/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.capstonecckma.repositories;
+
+import com.example.capstonecckma.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
