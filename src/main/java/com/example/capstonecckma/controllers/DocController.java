e8f70922d9563c4d5d6fe56c4d70a59726b89b45
leec89 <leec89@gmail.com>
2022-08-18 09:41:32 -0500

    feat: finish up multifile upload

--- src/main/java/com/example/capstonecckma/controllers/DocController.java
+++ src/main/java/com/example/capstonecckma/controllers/DocController.java
@@ -33,8 +33,8 @@ public class DocController {
         return "multiupload";
     }
 
-    @PostMapping("/multiupload")
-    public String uploadMultipleFiles(@RequestParam("file") MultipartFile[] files) {
+    @PostMapping("/uploadFiles")
+    public String uploadMultipleFiles(@RequestParam("files") MultipartFile[] files) {
         for (MultipartFile file: files) {
 
             List<Resource> resourceList = resourceDao.findAll();
