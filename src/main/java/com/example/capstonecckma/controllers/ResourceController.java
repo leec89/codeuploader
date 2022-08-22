c89301ee48eb44c78ccf68a84ca89da28fe2e56a
“Cory <coryurton@gmail.com>
2022-08-22 16:58:29 -0500

    fixed validation for creation of resources

--- src/main/java/com/example/capstonecckma/controllers/ResourceController.java
+++ src/main/java/com/example/capstonecckma/controllers/ResourceController.java
@@ -99,11 +99,6 @@ public class ResourceController {
         model.addAttribute("topic", list);
         return "resources/create";
     }
-    @GetMapping("/createtest")
-    public String getCreateTestForm(Model model) {
-        model.addAttribute("resource", new Resource());
-        return "resources/createtest";
-    }
 
     @PostMapping("/resources/create")
     public String postCreateForm(@ModelAttribute Resource resource) {
